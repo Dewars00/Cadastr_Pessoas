0b9e3ddcf62d5a1360e72b8efa19d3fb396a74d9
Alesson Massena <alessonmassena24@gmail.com>
2021-04-07 22:53:49 -0300

    Desenvolvimento dos menus
    Ainda não totalmente funcional.

--- cad.py
+++ cad.py
@@ -7,32 +7,34 @@ dados = {}
 v = 's'
 
 system('cls')
-print('=====Sistema para cadastro de pessoas=====')
-print()
+factor.topo('SISTEMA DE CADASTRO DE PESSOAS')
 input('Aperte ENTER para iniciar...')
+opt = factor.menu()
 system('cls')
 sleep(1)
-
-while True:
-    dados['Nome'] = str(input('Nome: '))
-    dados['Idade'] = int(input('Idade: '))
-    x = str(input('Sexo[M/F]: ')).upper()
-    factor.SexoVerificacao(x)       
-    dados['Sexo'] = x
-    pessoas.append(dados.copy())
-    dados.clear()
-    v = str(input('Deseja continuar[S/N]? ')).upper()
-    factor.DesejaContinuar(v)      
-    sleep(1)
-    system('cls')
-    if v == 'N':
-        break
-
-for i in range (0,len(pessoas)):
-    print('=' * 60)
-    for k,v in pessoas[i].items():
-        print(f'{k} \t====== \t{v}')
-    print('=' * 60)
-    print()
+if opt == 1:
+    while True:
+        factor.topo('SISTEMA DE CADASTRO DE PESSOAS')
+        dados['Nome'] = str(input('Nome: ')).upper()
+        dados['Idade'] = int(input('Idade: '))
+        x = str(input('Sexo[M/F]: ')).upper()
+        factor.SexoVerificacao(x)       
+        dados['Sexo'] = x
+        pessoas.append(dados.copy())
+        dados.clear()
+        v = str(input('Deseja continuar[S/N]? ')).upper()
+        factor.DesejaContinuar(v)      
+        sleep(1)
+        system('cls')
+        if v == 'N':
+            break
+elif opt == 2:
+    factor.topo('PESSOAS CADASTRADAS')
+    for i in range (0,len(pessoas)):
+        print('=' * 60)
+        for k,v in pessoas[i].items():
+            print(f'{k} \t====== \t{v}')
+        print('=' * 60)
+        print()
 
 
--- factor.py
+++ factor.py
@@ -1,5 +1,8 @@
+from time import sleep
+from os import system
+
+
 def SexoVerificacao(x):    #! Função que verifica se o valor inserido para sexo é válido
-    from time import sleep
     while True:
         if x in 'MF':
             break
@@ -10,13 +13,42 @@ def SexoVerificacao(x):    #! Função que verifica se o valor inserido para sex
 
     
 def DesejaContinuar(v):    #! Função que verifica se o valor de v é válido
-    from time import sleep
     while True:
         if v in 'SN':
             break
-        else:
-            print('===Entre com um valor válido!===')
-            sleep(1)
-            v = str(input('Deseja continuar[S/N]? ')).upper()
+        print(f'ERRO! ENTRE COM UM VALOR VÁLIDO')
+        sleep(1)
+        v = str(input('Deseja continuar[S/N]? ')).upper()
 
         
+def topo(msg):      #! Mensagem do topo da tela
+    print('='*60)
+    print(f'{msg}'.center(60))
+    print('='*60)
+    print()
+
+
+def notint(num):
+    while True:
+        if num in '123':
+                break
+        sleep(1)
+        print()
+        print('ERRO! ENTRE COM UM VALOR VÁLIDO')
+        sleep(1)
+        menu()
+    if num.isnumeric():
+        num = int(num)
+    return num
+
+
+def menu():
+    sleep(1)
+    system('cls')
+    topo('MENU PRINCIPAL')
+    print('[1] TELA DE CADASTROS\n[2] EXIBIR TODOS OS CADASTROS\n[3]EXCLUIR UM CADASTRO')
+    print()
+    opt = str(input('ESCOLHA UMA DAS OPÇÕES ACIMA... '))
+    opt = notint(opt)
+    return opt
+    sleep(1)
